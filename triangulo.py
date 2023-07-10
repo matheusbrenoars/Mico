c4f9ce4db10834ad2db56a9fbd1cee9ccb1605d5
Matheus Breno <128637637+matheusbrenoars@users.noreply.github.com>
2023-07-09 21:19:08 -0300

    Update and rename listaEncadeada.py to triangulo.py

---
+++ triangulo.py
@@ -0,0 +1,19 @@
+class Triangulo:
+    def __init__(self, v0, v1, v2):
+        self.v0 = v0
+        self.v1 = v1
+        self.v2 = v2
+        
+    def __str__(self):
+        return f"[{self.v0}, {self.v1}, {self.v2}]"
+        
+    
+    
+class Quadrado:
+    def __init__(self, v0, v1, v2, v3):
+        self.v0 = v0
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
+
+   
